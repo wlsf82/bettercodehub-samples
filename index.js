9a63b9ec5e18874bafb82ba4d3148853af43e5ef
Walmyr Filho <wlsf82@gmail.com>
2019-01-04 02:45:33 +0100

    Remove unnecessary `console.log()` from main code

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ function printDevInfo(firstName, lastName, twitterUser, githubUser) {
   return `${firstName} ${lastName}, ${twitterUser}, ${githubUser}`;
 }
 
-console.log(printDevInfo("Walmyr", "Filho", "@walmyrlimaesilv", "wlsf82"));
-
 module.exports = printDevInfo;
