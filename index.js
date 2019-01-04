2f39b9f8f733e8e9fd5a6ba9f420057fe4fa8f07
Walmyr Filho <wlsf82@gmail.com>
2019-01-04 02:22:09 +0100

    Create first sample
    Starting with a bad practice to clean it up in a future commit.

---
+++ index.js
@@ -0,0 +1,5 @@
+function printDevInfo(firstName, lastName, twitterUser, githubUser) {
+  return `${firstName} ${lastName}, ${twitterUser}, ${githubUser}`;
+}
+
+console.log(printDevInfo("Walmyr", "Filho", "@walmyrlimaesilv", "wlsf82"));
