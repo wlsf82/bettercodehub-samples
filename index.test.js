f48d7a8f832c0bea003b116720aa964b87c51120
Walmyr Filho <wlsf82@gmail.com>
2019-01-04 02:43:10 +0100

    Create unit test for printDevInfo function
    This will enables me to refactor the code without breaking it.

---
+++ index.test.js
@@ -0,0 +1,26 @@
+const test = require("tape");
+
+const printDevInfo = require("./index.js");
+
+test("returns correct info for dev: Walmyr", t => {
+  const info = {
+    firstName: "Walmyr",
+    lastName: "Filho",
+    twitter: "@walmyrlimaesilv",
+    github: "wlsf82"
+  };
+
+  const result = printDevInfo(
+    info.firstName,
+    info.lastName,
+    info.twitter,
+    info.github
+  );
+
+  const expected = `${info.firstName} ${info.lastName}, ${
+    info.twitter
+  }, ${info.github}`;
+
+  t.plan(1);
+  t.equal(result, expected);
+});
